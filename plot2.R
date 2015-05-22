988bf3dea25d28e6bd0ccbcf790b1ae734932d90
yiptsangkin <867584522@qq.com>
2015-05-22 17:59:07 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,12 +1,12 @@
-#�����ļ�
-#NEI <- readRDS("summarySCC_PM25.rds")
-#SCC <- readRDS("Source_Classification_Code.rds")
-#ɸѡ���Ͷ���Ħ�е�����
+#导入文件
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+#筛选出巴尔的摩市的数据
 NEI_BDM <- subset(NEI,NEI$fips =="24510")
-#�ֱ��ȡ1999�굽2008����ŷ�����
+#分别获取1999年到2008年的排放总量
 NEI_year <- tapply(X = NEI_BDM$Emissions, INDEX = NEI_BDM$year, FUN = sum)
-#�û���ϵͳ��������ͼ
+#用基础系统画出折线图
 png(filename = "plot1.png")
-plot(NEI_year,xlab="���",ylab=expression(PM[2.5]*"�ŷ�����"),pch=19,col="blue",type="b")
-title(expression("�����Ͷ���Ħ�н���"*PM[2.5]*"�ŷ���������"))
+plot(NEI_year,xlab="年份",ylab=expression(PM[2.5]*"排放总量"),pch=19,col="blue",type="b")
+title(expression("美国巴尔的摩市近年"*PM[2.5]*"排放总量趋势"))
 dev.off()
